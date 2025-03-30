5eedf6826dabe6686abdf1704e038c2ffdf61080
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-30 17:23:13 +0200

    chore: initialize Spring Boot application entry point in GyakorlatApplication.java

---
+++ demo/src/main/java/com/gyakorlas/demo/GyakorlatApplication.java
@@ -0,0 +1,13 @@
+package com.gyakorlas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyakorlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyakorlatApplication.class, args);
+	}
+
+}
