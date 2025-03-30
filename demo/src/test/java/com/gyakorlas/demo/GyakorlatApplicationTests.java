8cc820b6c48421adf9a03762f265c7ffc8b74058
Balog Sebastian Máté <sebimbalog@gmail.com>
2025-03-30 17:17:14 +0200

    test: ensure application context loads successfully during startup

---
+++ demo/src/test/java/com/gyakorlas/demo/GyakorlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyakorlas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyakorlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
